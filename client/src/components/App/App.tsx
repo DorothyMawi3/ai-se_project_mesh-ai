7d2057cac8471a427fe84918685914b3d5b0c031
Dorothy Thawm Luai Mawi <dorothy.ljk@gmail.com>
2026-06-27 21:51:25 -0400

    Include Intro and Chat in App

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import AppLayout from '../AppLayout/AppLayout';
+import Intro from '../../pages/Intro/Intro';
+import Chat from '../../pages/Chat/Chat';
 import KnowledgeBase from '../../pages/KnowledgeBase/KnowledgeBase';
 
 function App() {
   return (
     <div className="app">
       <AppLayout>
+        <Intro />
         <KnowledgeBase />
+        <Chat />
       </AppLayout>
     </div>
   );
